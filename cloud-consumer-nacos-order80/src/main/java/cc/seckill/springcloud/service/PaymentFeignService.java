76347bca8b919a171e17c7d9a063cc0be1badf5a
haiqiang0225 <haiqiang0225@gmail.com>
2022-05-06 11:04:24 +0800

    Add: sentinel support

---
+++ cloud-consumer-nacos-order80/src/main/java/cc/seckill/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,13 @@
+package cc.seckill.springcloud.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+
+/**
+ * description: PaymentFeignService <br>
+ * date: 2022/5/6 09:33 <br>
+ * author: hq <br>
+ * version: 1.0 <br>
+ */
+public interface PaymentFeignService {
+}
